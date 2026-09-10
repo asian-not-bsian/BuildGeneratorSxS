70a889fc089c1baae72a55de91a2833058b7a9d4
asian-not-bsian <35791025+asian-not-bsian@users.noreply.github.com>
2026-09-09 21:59:45 -0400

    Delete BuildGeneratorSxS-complete-v7.1.0 directory

--- BuildGeneratorSxS-complete-v7.1.0/js/modules/chaos-realm.js
+++
@@ -1,1 +0,0 @@
-export function init(app){const b=document.getElementById('cancelChaosV7');if(b&&!b.dataset.moduleWired){b.dataset.moduleWired='1';b.addEventListener('click',()=>app.cancel('chaos'))}}
